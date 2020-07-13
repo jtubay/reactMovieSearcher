825701341c0857eadc9734d83b2d8df4b395fcb2
Jean Tubay <jean.tubay.92@gmail.com>
2020-07-12 23:09:52 -0400

    adding search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Search from './component/Search.jsx'
 
 export default function App() {
     return(
         <div>
             <header>
-
                 <h1>Movie Database</h1>
             </header>
+            <main>
+                <Search />
+            </main>
         </div>
     )
 }
---
+++ src/component/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <section className="searchbox-wrap">
+
+        </section>
+    )
+}
+
+export default Search
