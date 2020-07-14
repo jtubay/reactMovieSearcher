b5431df2875f3bce15d19fcee4ed02c72d9cdbe0
Jean Tubay <jean.tubay.92@gmail.com>
2020-07-13 21:26:13 -0400

    added styling to search component

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Search() {
     return (
         <section className="searchbox-wrap">
-
+            <input type="text" placeholder="Search for a movie" className="searchbox"/>
         </section>
     )
 }
