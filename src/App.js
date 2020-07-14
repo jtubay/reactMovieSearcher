6e9fdfc06ae9770f2c0d4b814cf83ffdddeb820d
Jean Tubay <jean.tubay.92@gmail.com>
2020-07-13 21:43:05 -0400

    added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Search from './component/Search.jsx'
 
 export default function App() {
+    const [state, setState] = useState({
+        s: "",
+        results: [],
+        selected: {}
+    })
+    const apiURL = "https://www.omdbapi.com/?apikey=trilogy";
+
+    const handleInput = e => {
+        let s = e.target.value;
+
+        setState(prevState => {
+            return { ...prevState, s: s}
+        });
+
+        console.log(state.s)
+
+    }
+
     return(
+        
         <div>
             <header>
                 <h1>Movie Database</h1>
             </header>
             <main>
-                <Search />
+                <Search handleInput = {handleInput}/>
             </main>
         </div>
     )
